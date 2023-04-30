be424648ad3f36bd2cda900c7a771640e244d895
Mauricio Rojas Barrientos <mrojasb2000@gmail.com>
2023-04-30 17:13:24 -0400

    chore: refactor icons and Fluent ui

--- lib/screens/botton_bar.dart
+++ lib/screens/botton_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -22,11 +23,22 @@ class _BottonBarState extends State<BottonBar> {
           selectedItemColor: Colors.blueGrey,
           unselectedItemColor: const Color(0xFF526480),
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
             BottomNavigationBarItem(
-                icon: Icon(Icons.airplane_ticket), label: "Ticket"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
+                icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+                label: "Ticket"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+                label: "Profile")
           ]),
     );
   }
