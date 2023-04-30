69fc19b119624ff14c334c4a2fcc72f2707d3ed3
Mauricio Rojas Barrientos <mrojasb2000@gmail.com>
2023-04-30 15:24:21 -0400

    feta: basic definition flutter project

---
+++ lib/screens/botton_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class BottonBar extends StatefulWidget {
+  const BottonBar({super.key});
+
+  @override
+  State<BottonBar> createState() => _BottonBarState();
+}
+
+class _BottonBarState extends State<BottonBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
