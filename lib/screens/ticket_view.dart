39fa78437f5e6d35a0e415fb8bd5f74ad2bf6ad7
Mauricio Rojas Barrientos <mrojasb2000@gmail.com>
2023-04-30 22:41:05 -0400

    chore: add widgets circle

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_booktickets_app/utils/app_layout.dart';
 import 'package:gap/gap.dart';
 import '../utils/app_styles.dart';
+import '../widgets/thick_container.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({super.key});
@@ -29,7 +30,33 @@ class TicketView extends StatelessWidget {
                   Text("NYC",
                       style:
                           Styles.headLineStyle3.copyWith(color: Colors.white)),
-                  const Spacer(),
+                  Expanded(child: Container()),
+                  const ThickContainer(),
+                  Expanded(
+                      child: SizedBox(
+                          height: 24,
+                          child: LayoutBuilder(
+                            builder: (BuildContext context,
+                                BoxConstraints constraints) {
+                              return Flex(
+                                direction: Axis.horizontal,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: List.generate(
+                                  (constraints.constrainWidth() / 6).floor(),
+                                  (index) => const SizedBox(
+                                    width: 3,
+                                    height: 1,
+                                    child: DecoratedBox(
+                                        decoration:
+                                            BoxDecoration(color: Colors.white)),
+                                  ),
+                                ),
+                              );
+                            },
+                          ))),
+                  const ThickContainer(),
+                  Expanded(child: Container()),
                   Text("LDN",
                       style:
                           Styles.headLineStyle3.copyWith(color: Colors.white)),
