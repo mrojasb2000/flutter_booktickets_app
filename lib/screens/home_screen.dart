e9fd59765cb199dd9b5b709a3542dc8649747a91
Mauricio Rojas Barrientos <mrojasb2000@gmail.com>
2023-04-30 20:36:37 -0400

    feat: add search box

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -46,6 +47,21 @@ class HomeScreen extends StatelessWidget {
                                 image: AssetImage("assets/images/img_1.png"))),
                       )
                     ],
+                  ),
+                  const Gap(25),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color(0xFFF4F6FD)),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 12),
+                    child: Row(
+                      children: [
+                        const Icon(FluentSystemIcons.ic_fluent_search_filled,
+                            color: Color(0xFFBFC205)),
+                        Text("Search", style: Styles.headLineStyle4)
+                      ],
+                    ),
                   )
                 ],
               ),
