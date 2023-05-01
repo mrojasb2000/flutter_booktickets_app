b435876d8446ef94138bbc5b41848c90c15d800e
Mauricio Rojas Barrientos <mrojasb2000@gmail.com>
2023-05-01 00:01:38 -0400

    chore: add icons white

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_booktickets_app/utils/app_layout.dart';
+import 'package:gap/gap.dart';
 import '../utils/app_styles.dart';
 import '../widgets/thick_container.dart';
 
@@ -14,7 +15,8 @@ class TicketView extends StatelessWidget {
       height: 200,
       child: Container(
         margin: const EdgeInsets.only(left: 16),
-        child: Column(children: [
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          /* showing the blue part of the card/ticket */
           Container(
             decoration: const BoxDecoration(
                 color: Color(0xFF526799),
@@ -71,11 +73,11 @@ class TicketView extends StatelessWidget {
                           Styles.headLineStyle4.copyWith(color: Colors.white)),
                 ],
               ),
+              const Gap(5),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   SizedBox(
-                    width: 100,
                     child: Text('New-York',
                         style: Styles.headLineStyle3
                             .copyWith(color: Colors.white)),
@@ -83,10 +85,46 @@ class TicketView extends StatelessWidget {
                   Text('8H 30M',
                       style:
                           Styles.headLineStyle3.copyWith(color: Colors.white)),
+                  Text('London',
+                      style:
+                          Styles.headLineStyle3.copyWith(color: Colors.white)),
                 ],
               )
             ]),
           )
+          /* showing the orage part of the card/ticket */
+          ,
+          Container(
+              color: Styles.orageColor,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                          )),
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                          )),
+                    ),
+                  )
+                ],
+              ))
         ]),
       ),
     );
