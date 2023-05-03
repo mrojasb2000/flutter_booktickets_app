d6919549fae7f54f613f7c0ec25c5f0990d6269f
Mauricio Rojas Barrientos <mrojasb2000@gmail.com>
2023-05-02 20:53:14 -0400

    chore: add options selector

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -8,14 +8,40 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
     return Scaffold(
         backgroundColor: Styles.bgColor,
         body: ListView(
-          children: [
-            Gap(AppLayout.getHeight(40)),
-            Text("What are\nyou looking for?",
-                style: Styles.headLineStyle1.copyWith(fontSize: 35))
-          ],
-        ));
+            padding: EdgeInsets.symmetric(
+                horizontal: AppLayout.getWidth(20),
+                vertical: AppLayout.getHeight(35)),
+            children: [
+              Gap(AppLayout.getHeight(40)),
+              Text("What are\nyou looking for?",
+                  style: Styles.headLineStyle1
+                      .copyWith(fontSize: AppLayout.getHeight(35))),
+              Gap(AppLayout.getHeight(20)),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
+                  color: const Color(0xFFF4F6FD),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: size.width * .44,
+                      padding: EdgeInsets.symmetric(
+                          vertical: AppLayout.getHeight(7)),
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(AppLayout.getHeight(50)),
+                        color: Colors.white,
+                      ),
+                      child: const Text("Airline tickets"),
+                    ),
+                  ],
+                ),
+              )
+            ]));
   }
 }
