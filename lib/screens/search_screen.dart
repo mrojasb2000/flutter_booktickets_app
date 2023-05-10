ef92693e87422b579a86085ca4cc6dcd32e5fdc9
Mauricio Rojas Barrientos <maalroja@bancolombia.com.co>
2023-05-10 18:12:07 -0400

    chore: add departure block

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -21,29 +21,66 @@ class SearchScreen extends StatelessWidget {
                   style: Styles.headLineStyle1
                       .copyWith(fontSize: AppLayout.getHeight(35))),
               Gap(AppLayout.getHeight(20)),
+              FittedBox(
+                child: Container(
+                  padding: const EdgeInsets.all(3.5),
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.circular(AppLayout.getHeight(50)),
+                    color: const Color(0xFFF4F6FD),
+                  ),
+                  child: Row(
+                    children: [
+                      /* airline tickets */
+                      Container(
+                        width: size.width * .44,
+                        padding: EdgeInsets.symmetric(
+                            vertical: AppLayout.getHeight(7)),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.horizontal(
+                              left: Radius.circular(AppLayout.getHeight(50))),
+                          color: Colors.white,
+                        ),
+                        child: const Center(
+                          child: Text("Airline tickets"),
+                        ),
+                      ),
+                      /* hotels */
+                      Container(
+                        width: size.width * .44,
+                        padding: EdgeInsets.symmetric(
+                            vertical: AppLayout.getHeight(7)),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.horizontal(
+                              right: Radius.circular(AppLayout.getHeight(50))),
+                          color: Colors.transparent,
+                        ),
+                        child: const Center(
+                          child: Text("Hotels"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Gap(AppLayout.getHeight(25)),
               Container(
-                padding: const EdgeInsets.all(3.5),
+                padding: EdgeInsets.symmetric(
+                    vertical: AppLayout.getWidth(12),
+                    horizontal: AppLayout.getHeight(12)),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
-                  color: const Color(0xFFF4F6FD),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      width: size.width * .44,
-                      padding: EdgeInsets.symmetric(
-                          vertical: AppLayout.getHeight(7)),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.horizontal(
-                            left: Radius.circular(AppLayout.getHeight(50))),
-                        color: Colors.white,
-                      ),
-                      child: const Center(
-                        child: Text("Airline tickets"),
-                      ),
-                    ),
-                  ],
-                ),
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.circular(AppLayout.getWidth(10))),
+                child: Row(children: [
+                  const Icon(Icons.flight_takeoff_rounded,
+                      color: Color(0xFFBFC2DF)),
+                  Gap(AppLayout.getWidth(10)),
+                  Text(
+                    'Departure',
+                    style: Styles.textStyle,
+                  ),
+                ]),
               )
             ]));
   }
